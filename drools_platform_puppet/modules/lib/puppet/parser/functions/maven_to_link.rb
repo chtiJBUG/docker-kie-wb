66c33368e1957807e6ed2d4a11f7d46e7a7ef3e0
Nicolas Héron <nicolas.heron@pymma-software.com>
2015-02-01 11:30:56 +0100

    bug correction on maven to link

--- drools_platform_puppet/modules/lib/puppet/parser/functions/maven_to_link.rb
+++ drools_platform_puppet/modules/lib/puppet/parser/functions/maven_to_link.rb
@@ -4,24 +4,29 @@ module Puppet::Parser::Functions
       link=args[0]
       build_link=link.clone
       link.include?("SNAPSHOT") ? version='snapshots' : version='releases'
+      link.include?("war") ? findextension='war</a>' : findextension='jar</a>'
       link.include?("war") ? extension='war' : extension='jar'
-
+     # debug("link="+link)
+     # debug("extension="+extension)
       tmp = link.split(':')
       build_link="https://oss.sonatype.org/content/repositories/#{version}/"
       build_link="#{build_link}#{tmp[0].gsub('.','/')}/#{tmp[1]}/"
-      if link.include?("SNAPSHOT")
+     if version== 'snapshots'
+     
+      # debug("2-------rrrrrr------")
       	temp = link.scan(/\d.\d.\d-#{version.upcase.slice(0..-2)}/)
      	temp = temp.map(&:inspect).join('').slice(1..-2)
       	build_link="#{build_link}#{temp}/"
-	  	print build_link
+	  #	debug("2bis build_link="+build_link)
       	http = `wget #{build_link} -O -`
-.      	grep = http.split("\n").grep(/#{extension}/)
-
+      	
+      	grep = http.split("\n").grep(/#{findextension}/)
       	i=0
+      #	debug("http=("+http+")")
       	grep.each do |truc|
         	grep[i]=truc.scan(/\d.\d.\d-\d\d\d\d\d\d\d\d.\d\d\d\d\d\d-\d+/).map(&:inspect).join('')
         	supp = (grep[i].length - 50) /2
-      
+       #     debug("grep[i]="+grep[i]+" i="+i.to_s)
         	grep[i]=truc.scan(/\d.\d.\d-\d\d\d\d\d\d\d\d.\d\d\d\d\d\d-\d+/).map(&:inspect).join('').slice!(1..23 + supp)
         	i+=1
       	end
@@ -31,15 +36,16 @@ module Puppet::Parser::Functions
 
       	i=0
       	num.each do |trac|
+      	#    debug("trac="+trac)
         	num[i]=trac.delete(".").delete("-")
         	i+=1
      	end
 
       	retval="#{build_link}#{tmp[1]}-#{grep[num.index(num.max)]}.#{extension}"
       else
-        retval="#{build_link}/#{tmp[2]}/#{tmp[1]}-#{tmp[2]}.#{extension}"
-      end 
-    	retval
+          retval="#{build_link}/#{tmp[2]}/#{tmp[1]}-#{tmp[2]}.#{extension}"
+       end 
+       retval
     end
     
 end
